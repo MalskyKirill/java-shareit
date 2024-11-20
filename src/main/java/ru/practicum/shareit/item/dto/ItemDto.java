989ce5c04399aff4b281a68ba30942657381f7f0
Kirill Malskiy <sabbat@mail.ru>
2024-11-20 17:27:31 +0300

    добавил dto для user и item

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,19 @@
 package ru.practicum.shareit.item.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
 /**
  * TODO Sprint add-controllers.
  */
 public class ItemDto {
+    private Long id;
+    @NotBlank(message = "item name can't be null, empty or blank")
+    private String name;
+    @NotBlank(message = "description can't be null, empty or blank")
+    private String description;
+    private Boolean available;
+    private ItemRequest request;
 }
---
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public class UserDto {
+    private Long id;
+    @NotBlank(message = "user name can't be null, empty or blank")
+    @Pattern(regexp = "\\S+", message = "user name mast contain no whitespace")
+    private String name;
+    @NotBlank(message = "email can't be null, empty or blank")
+    @Email(message = "email should be valid")
+    private String email;
+}
