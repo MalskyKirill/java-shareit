d7872cb1fd80d649390f6b9a7529f3a2d8e1a35d
Kirill Malskiy <sabbat@mail.ru>
2024-11-20 22:13:44 +0300

    добавил класс UserMapper

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -3,7 +3,9 @@ package ru.practicum.shareit.user.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.Data;
 
+@Data
 public class UserDto {
     private Long id;
     @NotBlank(message = "user name can't be null, empty or blank")
---
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.user.mapper;
+
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+
+public class UserMapper {
+    public UserDto mapToUserDto(User user) {
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setName(user.getName());
+        userDto.setEmail(user.getEmail());
+        return userDto;
+    }
+
+
+    public User mapToUser(User user) {
+        User newUser = new User();
+        newUser.setId(user.getId());
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+        return newUser;
+    }
+}
